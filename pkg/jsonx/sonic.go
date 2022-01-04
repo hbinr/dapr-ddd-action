7de24587f0192dec7214035dc3b288f30b89e9a5
段浩斌 <duanhaobin@duanhaobindeMacBook-Pro.local>
2022-01-04 18:40:39 +0800

    feat(jsonx): 兼容windows

--- pkg/jsonx/sonic.go
+++ pkg/jsonx/sonic.go
@@ -1,13 +1,24 @@
 package jsonx
 
 import (
+	"encoding/json"
 	"errors"
+	"runtime"
 
 	"github.com/bytedance/sonic"
 )
 
 func Marshal(val interface{}) ([]byte, error) {
-	return sonic.Marshal(val)
+	switch runtime.GOOS {
+	case "windows":
+		return json.Marshal(val)
+	case "darwin":
+		return sonic.Marshal(val)
+	case "linux":
+		return sonic.Marshal(val)
+	default:
+		return nil, errors.New("jsonx: unknown os")
+	}
 }
 
 func Unmarshal(buf []byte, val interface{}) error {
@@ -15,5 +26,15 @@ func Unmarshal(buf []byte, val interface{}) error {
 		return errors.New("jsonx: empty data to unmarshal")
 	}
 
-	return sonic.Unmarshal(buf, val)
+	switch runtime.GOOS {
+	case "windows":
+		return json.Unmarshal(buf, val)
+	case "darwin":
+		return sonic.Unmarshal(buf, val)
+	case "linux":
+		return sonic.Unmarshal(buf, val)
+	default:
+		return errors.New("jsonx: unknown os")
+	}
+
 }
