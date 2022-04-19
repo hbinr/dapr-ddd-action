068dd4f0319e3393a04a968e427a518f311b724a
hbinr <dhb1807227469@.163.com>
2022-04-19 14:55:38 +0800

    fix: fiber custom error handle

--- pkg/server/http.go
+++ pkg/server/http.go
@@ -1,29 +1,30 @@
 package server
 
 import (
-	"github.com/dapr-ddd-action/pkg/errorx"
 	"github.com/dapr-ddd-action/pkg/jsonx"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"go.uber.org/zap"
 )
 
 func NewHttpServer(servers ...Server) *fiber.App {
 	config := fiber.Config{
 		DisableStartupMessage: true,
 		// custom error handler
-		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			zap.L().Error("internal err", zap.Error(err))
-			// fiber error
-			fiberNativeErr, ok := err.(*fiber.Error)
-			if ok {
-				return fiberNativeErr
-			}
+		// ErrorHandler: func(c *fiber.Ctx, err error) error {
+		// 	zap.L().Error("internal err: ", zap.Error(err))
+		// 	// fiber error
+		// 	fiberNativeErr, ok := err.(*fiber.Error)
+		// 	if ok {
+		// 		zap.L().Sugar().Infof("template string%+v：", fiberNativeErr)
 
-			// custom error
-			errx := errorx.From(err)
-			return c.Status(errx.Code).JSON(errx)
-		},
+		// 		return fiberNativeErr
+		// 	}
+		// 	zap.L().Sugar().Infof("template string%+v：", fiberNativeErr)
+
+		// 	// custom error
+		// 	errx := errorx.From(err)
+		// 	return c.Status(errx.Code).JSON(errx)
+		// },
 		// custom  json encode or decode
 		JSONEncoder: jsonx.Marshal,
 		JSONDecoder: jsonx.Unmarshal,
